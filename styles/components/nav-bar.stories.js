22408b045289998cd64fca9585d7c2e142a0788c
Christoph Timm <quaquaro@gmail.com>
2022-01-18 15:18:58 +0100

    Add some stories

---
+++ styles/components/nav-bar.stories.js
@@ -0,0 +1,14 @@
+import "./nav-bar.css";
+
+export default {
+  title: "Component/Navigation",
+};
+
+export const Default = () =>
+  `<nav class="nav-bar flex justify-end">
+  <a class="nav-bar__button ml-15" href="">Kontakt</a>
+  <a class="nav-bar__button ml-15" href="">Über mich</a>
+  <a class="nav-bar__button ml-15" href="">Downloads</a>
+  </nav>`;
+
+  
